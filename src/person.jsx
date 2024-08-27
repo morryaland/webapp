1dd5b2cb58d6d8697c01fbfa1f77e74f27d04862
morryaland <vozhov.matvey@gmail.com>
2024-08-27 23:02:18 +0300

    style

--- src/person.jsx
+++ src/person.jsx
@@ -1,11 +1,25 @@
 const React = require("react");
+const { default: styled } = require("styled-components");
+
+const Base = styled.div`
+  border: 1px solid black;
+`;
+
+const Table = styled(Base).attrs({ as: 'table' })`
+  border-collapse: collapse;
+`;
+
+const TableRow = styled(Base).attrs({ as: 'tr' })``;
+const TableData = styled(Base).attrs({ as: 'td' })``;
+const TableHeader = styled(Base).attrs({ as: 'th' })``;
 
 function Person(props)
 {
-  return <tr>
-    <td>Name: {props.name}</td>
-    <td>Age: {props.age}</td>
-  </tr>;
+  return <TableRow>
+    <TableData>{props.id}</TableData>
+    <TableData>{props.name}</TableData>
+    <TableData>{props.age}</TableData>
+  </TableRow>;
 }
 
 function PersonList(props)
@@ -17,14 +31,23 @@ function PersonList(props)
       method: "GET",
       headers: { "Accept": "application/json" }
     });
+    if (!response.ok)
+      return;
     let p = await response.json();
-    let personList = [];
-    p.forEach(person => personList.push(<Person name={person.name} age={person.age}/>));
+    let personList = p.map(person => <Person key={person.id} id={person.id} name={person.name} age={person.age}/>);
     SetPersonComonentList(personList);
   }
-  return (<div><table>
-    {personComponentList}
-  </table>
+  return (
+  <div><Table>
+    <thead>
+      <TableRow>
+        <TableHeader>ID</TableHeader> <TableHeader>NAME</TableHeader> <TableHeader>AGE</TableHeader>
+      </TableRow>
+    </thead>
+    <tbody>
+      {personComponentList}
+    </tbody>
+  </Table>
   <button onClick={GetPerson}>Get Data</button>
   </div>)
 }
